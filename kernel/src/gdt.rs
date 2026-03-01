430cca174a51e32b647bcc4e3a4c892cc1c8164d
RAFSuNX <git@rafsunx.com>
2026-03-01 14:01:28 +0600

    feat: GDT and TSS setup with double fault stack

---
+++ kernel/src/gdt.rs
@@ -0,0 +1,39 @@
+use x86_64::VirtAddr;
+use x86_64::structures::tss::TaskStateSegment;
+use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector};
+use spin::Lazy;
+
+pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+const STACK_SIZE: usize = 4096 * 5;
+
+static TSS: Lazy<TaskStateSegment> = Lazy::new(|| {
+    let mut tss = TaskStateSegment::new();
+    tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
+        static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+        let stack_start = VirtAddr::from_ptr(core::ptr::addr_of!(STACK));
+        stack_start + STACK_SIZE as u64
+    };
+    tss
+});
+
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector:  SegmentSelector,
+}
+
+static GDT: Lazy<(GlobalDescriptorTable, Selectors)> = Lazy::new(|| {
+    let mut gdt = GlobalDescriptorTable::new();
+    let code_selector = gdt.append(Descriptor::kernel_code_segment());
+    let tss_selector  = gdt.append(Descriptor::tss_segment(&TSS));
+    (gdt, Selectors { code_selector, tss_selector })
+});
+
+pub fn init() {
+    use x86_64::instructions::tables::load_tss;
+    use x86_64::instructions::segmentation::{CS, Segment};
+    GDT.0.load();
+    unsafe {
+        CS::set_reg(GDT.1.code_selector);
+        load_tss(GDT.1.tss_selector);
+    }
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,6 +3,7 @@
 #![feature(abi_x86_interrupt)]
 
 mod serial;
+mod gdt;
 
 use bootloader_api::{BootInfo, entry_point};
 use core::panic::PanicInfo;
@@ -12,6 +13,8 @@ entry_point!(kernel_main);
 fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
     serial::init();
     serial_println!("OCI Kernel 0.1.0 booting...");
+    gdt::init();
+    serial_println!("[OK] GDT");
     loop {}
 }
 
